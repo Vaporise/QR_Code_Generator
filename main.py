3f74d345ae8e6025beb01ff76c906955f983013c
Vaporise <draxdragonfire@googlemail.com>
2025-12-01 14:18:19 +0900

    Added the main code to convert and entered piece of data into a QR Code (there is no confirming yet of correct input variables)

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
+import qrcode
+
 def main():
 
+    data = input("Please enter web address:").lower()
 
+    qr = qrcode.QRCode(
+        version=1, #Controls the size o the QR code
+        error_correction=qrcode.constants.ERROR_CORRECT_L, #Error correction level
+        box_size=10, # Size of each box (pixel) in the QR code
+        border =4, #Thickness of the border
+    )
+
+    qr.add_data(data)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+
+    img.save("my_qrcode.png")
+
+    print("QR code generated and saved as my_qrcode.png")
 
 if __name__ == "__main__":
     main() 
